1e782aba2d09cb7340ca877d33df43b0c7e9a329
Gleb <qloy434@gmail.com>
2025-07-29 21:31:51 +0500

    work 2

---
+++ my-app/src/App/components/users.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import User from "./user";
+import Pagination from "./pagination";
+import { paginate } from "../utils/paginate";
+
+const Users = ({ users, ...rest }) => {
+    const count = users.length;
+    const pageSize = 4;
+    const [currentPage, setCurrentPage] = useState(1);
+    const handlePageChange = (pageIndex) => {
+        setCurrentPage(pageIndex);
+    };
+    const userCrop = paginate(users, currentPage, pageSize);
+    return (
+        <>
+            {count > 0 && (
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Имя</th>
+                            <th scope="col">Качества</th>
+                            <th scope="col">Провфессия</th>
+                            <th scope="col">Встретился, раз</th>
+                            <th scope="col">Оценка</th>
+                            <th scope="col">Избранное</th>
+                            <th />
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {userCrop.map((user) => (
+                            <User {...rest} {...user} key={user._id} />
+                        ))}
+                    </tbody>
+                </table>
+            )}
+            <Pagination
+                itemsCount={count}
+                currentPage={currentPage}
+                pageSize={pageSize}
+                onPageChange={handlePageChange}
+            />
+        </>
+    );
+};
+
+export default Users;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,11 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
-import Users from "./components/users";
-
-const App = () => {
-  return <Users />;
-};
+import App from "./App/app";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
