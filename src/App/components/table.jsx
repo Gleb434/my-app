9b1a25d7a50379ac9d3b7791204b36082fe366f9
Gleb <qloy434@gmail.com>
2025-08-06 20:20:25 +0500

    work 6

---
+++ src/App/components/table.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Table = ({ children }) => {
+    return <table className="table">{children}</table>;
+};
+Table.propTypes = {
+    children: PropTypes.array
+};
+
+export default Table;
